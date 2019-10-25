3b222cbed62fdb958c4d22c2b2c15f1059402529
Tran Nguyen Hai Thanh <thanh.trannguyenhai@axonactive.com>
2019-10-25 16:39:10 +0700

    Updates

---
+++ precache-manifest.6e3a45f7f0bb8c650b0417236f854fdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dee0da541dfd38c9941fef6cde52bb88",
+    "url": "/react-simple-notification/index.html"
+  },
+  {
+    "revision": "82eb88636ea1922def60",
+    "url": "/react-simple-notification/static/css/main.6d55fc08.chunk.css"
+  },
+  {
+    "revision": "3829c9c350a63cd7ef77",
+    "url": "/react-simple-notification/static/js/2.2ca31065.chunk.js"
+  },
+  {
+    "revision": "82eb88636ea1922def60",
+    "url": "/react-simple-notification/static/js/main.13525185.chunk.js"
+  },
+  {
+    "revision": "0b4c985f5e2ffd868c52",
+    "url": "/react-simple-notification/static/js/runtime~main.2087e25a.js"
+  }
+]);
